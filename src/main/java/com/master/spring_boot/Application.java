325cd46f1bb41988d5118f7f56ff0741ade632c6
farid shojaemehr <farid.juve@gmail.com>
2024-11-02 11:39:19 +0200

    implement rest controller

--- src/main/java/com/master/spring_boot/Application.java
+++ src/main/java/com/master/spring_boot/Application.java
@@ -2,12 +2,33 @@ package com.master.spring_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	public enum Gender {MALE, FEMALE};
+	public record Person(int id,String name,int age, Gender gender){}
+	public static List<Person> people = new ArrayList<>();
+	static {
+		people.add(new Person(1,"John",18,Gender.MALE));
+		people.add(new Person(2,"Jane",20,Gender.FEMALE));
+		people.add(new Person(3,"Bob",24,Gender.MALE));
+		people.add(new Person(4,"Mary",28,Gender.FEMALE));
+	}
+
+	@GetMapping
+	public List<Person> getPersons(){
+		return people;
 	}
 
 }
