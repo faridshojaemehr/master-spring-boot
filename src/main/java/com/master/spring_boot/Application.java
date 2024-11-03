476ca776306eadb2ad1d3460cc5b214da3ce3ad0
farid shojaemehr <farid.juve@gmail.com>
2024-11-03 02:46:42 +0200

    Jackson implement

--- src/main/java/com/master/spring_boot/Application.java
+++ src/main/java/com/master/spring_boot/Application.java
@@ -1,12 +1,16 @@
 package com.master.spring_boot;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.CommandLineRunner;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -24,12 +28,31 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner(ObjectMapper objectMapper) {
+		String personString = "{\"id\":1\"name\":\"farid\"}";
+        Person person = null;
+        try {
+            person = objectMapper.readValue(personString, Person.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(person);
+        try {
+            System.out.println(objectMapper.writeValueAsString(personString));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        return args -> {
+
+		};
+	};
 
 	public enum SortingOrder{DESC,ASC}
 	public enum Gender {MALE, FEMALE};
 	public static AtomicInteger idGenerator = new AtomicInteger();
 	public record PersonUpdate(String name,Integer age){}
-	public record Person(Integer id,String name,Integer age, Gender gender){}
+//	public record Person(Integer id,String name, Integer age, Gender gender){}
 	public static List<Person> people = new ArrayList<>();
 	static {
 		people.add(new Person(idGenerator.incrementAndGet(),"John",18,Gender.MALE));
@@ -51,7 +74,7 @@ public class Application {
 //		System.out.println("request => " + request.getLocalAddr());
 //		System.out.println(contentType);
 		List<Person> people1 = people.stream()
-				.sorted(sort == SortingOrder.DESC ? Comparator.comparing(Person::id).reversed() : Comparator.comparing(Person::id))
+				.sorted(sort == SortingOrder.DESC ? Comparator.comparing(Person::getId).reversed() : Comparator.comparing(Person::getId))
 				.collect(Collectors.toList());
 		return ResponseEntity.ok().body(people1);
 	}
@@ -62,30 +85,30 @@ public class Application {
 			@PathVariable("id") int id
 			){
 		Optional<Person> selectedPerson = people.stream()
-				.filter(person -> person.id.equals(id)).findFirst();
+				.filter(person -> person.getId().equals(id)).findFirst();
 		return ResponseEntity.ok().body(selectedPerson);
 	}
 
 
 	@PostMapping("/add")
 	public ResponseEntity<Person> addPerson(@RequestBody Person person) {
-		people.add(new Person(idGenerator.incrementAndGet(),person.name,person.age(),person.gender));
+		people.add(new Person(idGenerator.incrementAndGet(),person.getName(),person.getAge(),person.getGender()));
 		return ResponseEntity.ok().body(person);
 	}
 
 	@PutMapping("{id}")
 	public void updatePerson(@PathVariable int id, @RequestBody PersonUpdate personUpdate) {
 		people.stream()
-				.filter(el -> el.id.equals(id))
+				.filter(el -> el.getId().equals(id))
 				.findFirst()
 				.ifPresent(existingPerson -> {
 					var index = people.indexOf(existingPerson);
-					if (personUpdate.name != null && !personUpdate.name.isEmpty() && !personUpdate.name.equals(existingPerson.name) ) {
-						Person person = new Person(existingPerson.id,personUpdate.name,existingPerson.age(),existingPerson.gender);
+					if (personUpdate.name != null && !personUpdate.name.isEmpty() && !personUpdate.name.equals(existingPerson.getName()) ) {
+						Person person = new Person(existingPerson.getId(),personUpdate.name,existingPerson.getAge(),existingPerson.getGender());
 						people.set(index, person);
 					}
-					if (personUpdate.age != null && !personUpdate.age.equals(existingPerson.age) ) {
-						Person person = new Person(existingPerson.id,existingPerson.name,personUpdate.age(),existingPerson.gender);
+					if (personUpdate.age != null && !personUpdate.age.equals(existingPerson.getAge()) ) {
+						Person person = new Person(existingPerson.getId(),existingPerson.getName(),personUpdate.age(),existingPerson.getGender());
 						people.set(index, person);
 					}
 
@@ -105,7 +128,7 @@ public class Application {
 	public ResponseEntity<List<Person>> findPersonById(
 			@PathVariable("id") int id
 	){
-		people.removeIf(person -> person.id.equals(id));
+		people.removeIf(person -> person.getId().equals(id));
 		return ResponseEntity.ok().body(people);
 	}
 
---
+++ src/main/java/com/master/spring_boot/Person.java
@@ -0,0 +1,41 @@
+package com.master.spring_boot;
+
+public class Person {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private Application.Gender gender;
+
+    public Person(Integer id, String name, Integer age, Application.Gender gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Application.Gender getGender() {
+        return gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
+}
