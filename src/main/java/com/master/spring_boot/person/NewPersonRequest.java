8d7424f8973fd4b51b29b148023a964dcd2679a8
farid shojaemehr <farid.juve@gmail.com>
2024-11-04 02:46:40 +0200

    implement spring data jpa && hibernate && database h2

--- src/main/java/com/master/spring_boot/person/NewPersonRequest.java
+++ src/main/java/com/master/spring_boot/person/NewPersonRequest.java
@@ -2,14 +2,13 @@ package com.master.spring_boot.person;
 
 import com.master.spring_boot.validation.MyCustomValidation;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record NewPersonRequest(
-        @NotEmpty(message = "Name cannot be null or empty")
-        @MyCustomValidation(message = "The field cannot contain the value 'farid'.") String name,
-        @Min(value = 18,message = "Age must be grater than 18") Integer age,
-        @NotNull(message = "Gender must not be null") Gender gender,
+        @NotEmpty @MyCustomValidation  String name,
+        @NotNull Integer age,
+        @NotNull Gender gender,
         @Email String email
-) {}
+) {
+}
--- src/main/java/com/master/spring_boot/person/PersonRepository.java
+++ src/main/java/com/master/spring_boot/person/PersonRepository.java
@@ -1,35 +1,8 @@
 package com.master.spring_boot.person;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-
-
-@Repository
-public class PersonRepository {
-
-    public static final AtomicInteger idGenerator = new AtomicInteger();
-    private static List<Person> people = new ArrayList<>();
-
-    static {
-        people.add(new Person(idGenerator.incrementAndGet(), "John", 18, Gender.MALE, "john@example.com"));
-        people.add(new Person(idGenerator.incrementAndGet(), "Jane", 20, Gender.FEMALE, "jane@example.com"));
-        people.add(new Person(idGenerator.incrementAndGet(), "Bob", 24, Gender.MALE, "bob@example.com"));
-        people.add(new Person(idGenerator.incrementAndGet(), "Mary", 28, Gender.FEMALE, "mary@example.com"));
-        people.add(new Person(idGenerator.incrementAndGet(), "Rose", 18, Gender.MALE, "Rose@example.com"));
-        people.add(new Person(idGenerator.incrementAndGet(), "Ahmad", 20, Gender.FEMALE, "Ahmad@example.com"));
-        people.add(new Person(idGenerator.incrementAndGet(), "Ali", 24, Gender.MALE, "Ali@example.com"));
-        people.add(new Person(idGenerator.incrementAndGet(), "Saba", 28, Gender.FEMALE, "Saba@example.com"));
-
-    }
-
-    public AtomicInteger getIdGenerator() {
-        return idGenerator;
-    }
-
-    public List<Person>  getPeople() {
-        return people;
-    }
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+    boolean existsByEmail(String email);
+    Person findById(int id);
 }
